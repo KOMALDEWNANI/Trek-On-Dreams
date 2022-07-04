d4d4210a7c123d2c9177c8f1ab08b64c1416c69e
Adarsh Sharma <adarshballabh.sharma11@gmail.com>
2022-07-04 14:12:20 +0530

    client Setup Done

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app ">
+      <p className="text-red-700 text-3xl text-center">Let's start Trek On Dreams</p>
+    </div>
+  );
+}
+
+export default App;
