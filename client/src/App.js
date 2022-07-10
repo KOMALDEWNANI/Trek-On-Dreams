b496699061aadd466d45dfb3ed682887d4a39ec2
Adarsh Sharma <adarshballabh.sharma11@gmail.com>
2022-07-11 03:58:27 +0530

    Test Slider dumped

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import Carousel from './Components/Carousel';
+import Carousel from './Components/Carousel';
 import Catalogue from './Components/Catalogue';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
@@ -9,8 +9,8 @@ function App() {
     <div className="app bg-slate-200">
       <p className="text-red-700 text-3xl text-center bg-amber-200">Trek On Dreams in PROGRESS...</p>
       <Navbar/>
-      {/* <Carousel /> */}
-      <div className="flex">
+      <Carousel />
+      <div className="grid grid-cols-4 gap-10">
       <Catalogue
       link = "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJla2tpbmd8ZW58MHx8MHx8&w=1000&q=80"
       title = "Trek Name"
@@ -31,8 +31,6 @@ function App() {
       title = "Trek Name"
       description = "Trek description along side fees and perks."
       />
-      </div>
-      <div className="flex">
       <Catalogue
       link = "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJla2tpbmd8ZW58MHx8MHx8&w=1000&q=80"
       title = "Trek Name"
@@ -53,8 +51,6 @@ function App() {
       title = "Trek Name"
       description = "Trek description along side fees and perks."
       />
-      </div>
-      <div className="flex">
       <Catalogue
       link = "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJla2tpbmd8ZW58MHx8MHx8&w=1000&q=80"
       title = "Trek Name"
--- client/src/Components/Carousel.js
+++ client/src/Components/Carousel.js
@@ -1,1 +1,14 @@
 // ?To be completed
+import React from 'react'
+import pic from "../slider.gif"
+
+function Carousel() {
+  return (
+    <div className="ml-96 my-10 w-96 border-double border-2 rounded-lg border-black ">
+        <img src={pic}
+         alt="" />
+      </div>
+  )
+}
+
+export default Carousel
