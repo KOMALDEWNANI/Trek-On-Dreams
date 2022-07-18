040a9527529398bbb8a9cbe89ff3c0f3d12bfd84
yash23762 <64649849+yash23762@users.noreply.github.com>
2022-07-18 13:04:56 +0530

    Added another Carousel next the previous one and did some other minor changes.

---
+++ client/src/Components/Carousel2.js
@@ -0,0 +1,14 @@
+import { height, margin, width } from '@mui/system'
+import React from 'react'
+import pic from "../Carousel2.gif"
+
+function Carousel2() {
+  return (
+    <div >
+        <img style={{height: "250px", borderwidth:"40px", marginLeft:"800px", marginTop:"-300px"}} src={pic}
+         alt="" />
+      </div>
+  )
+}
+
+export default Carousel2
